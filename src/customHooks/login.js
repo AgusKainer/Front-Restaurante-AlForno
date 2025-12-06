6f242cc4eae104b3777e7489a5f0f13365601f62
agus kainer <puchidev98@gmail.com>
2025-12-06 16:15:47 -0300

    login arreglado

--- src/customHooks/login.js
+++ src/customHooks/login.js
@@ -2,7 +2,7 @@ const loginAdmin = async (usuario, password) => {
   const payload = { usuario, password }; // ✅ objeto plano
   console.log("Payload:", payload); // Verificá que sea { usuario: "admin", password: "1234" }
 
-  const res = await fetch("http://localhost:1000/login", {
+  const res = await fetch("https://back-restaurante-alforno-production.up.railway.app/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload), // ✅ esto no debería fallar
