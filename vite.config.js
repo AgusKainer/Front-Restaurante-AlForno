071298f965d0beed500fffe6dd4ef9edc6646272
agus kainer <puchidev98@gmail.com>
2025-12-06 16:05:57 -0300

    probando en railway, agregando para que funcione el front

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['front-restaurante-alforno-production.up.railway.app'],
+    port: process.env.PORT || 4173,
+    host: true
+  }
+
 });
